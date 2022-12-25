bb80df59f1db4f35ba0eb6e28c58399c3f80cced
imlakshaychauhan <imlakshaychauhan@gmail.com>
2022-12-25 14:24:35 +0530

    Adding Items to Redux Store DONE

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Cart from './pages/Cart'
 import Navbar from './components/Navbar'
+import { Provider } from 'react-redux'
+import store from './store/store';
 
 const App = () => {
   return (
-    <BrowserRouter>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<Home />} ></Route>
-            <Route path="/cart" element={<Cart />} ></Route>
-            <Route path="*" element={<Home />} ></Route>
-        </Routes>
-    </BrowserRouter>
+    <Provider store={store} >
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} ></Route>
+                <Route path="/cart" element={<Cart />} ></Route>
+                <Route path="*" element={<Home />} ></Route>
+            </Routes>
+        </BrowserRouter>
+    </Provider>
   )
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect }  from 'react'
+import { add } from '../store/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
     useEffect(() => {
         const fetchProducts = async () => {
             await fetch("https://fakestoreapi.com/products")
             .then((response) => response.json())
-            .then((data) => {
+            .then((data) => { 
                 console.log(data)
                 setProducts(data);
             });
         }
         fetchProducts();
     }, []);
+    const addProduct = (product) => {
+        dispatch(add(product));
+    }
   return (
     <div className='productsWrapper'>{
         products.map(product => (
@@ -20,7 +27,7 @@ const Products = () => {
                 <img src={product.image} alt="" />
                 <h4>{product.title}</h4>
                 <h5>$ {product.price}</h5>
-                <button className='btn'>Add to Cart</button>
+                <button onClick={() => addProduct(product)}  className='btn'>Add to Cart</button>
             </div>
         ))
     }</div>
