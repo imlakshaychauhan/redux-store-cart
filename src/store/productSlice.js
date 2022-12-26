74e0204c8d3473936f1c655057c1be8422d72092
imlakshaychauhan <imlakshaychauhan@gmail.com>
2022-12-26 18:36:14 +0530

    Added new Thunk Implementation with same working

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 // this is read only
 export const STATUSES = Object.freeze({
@@ -14,33 +14,54 @@ const productSlice = createSlice({
         status: STATUSES.IDLE,
     },
     reducers: {
-        setProducts(state, action) {
-            state.data = action.payload;
-        },
-        setStatus(state, action) {
-            state.status = action.payload;
-        }
+        // setProducts(state, action) {
+        //     state.data = action.payload;
+        // },
+        // setStatus(state, action) {
+        //     state.status = action.payload;
+        // }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state, action) => {
+                state.status = STATUSES.LOADING;
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.data = action.payload;
+                state.status = STATUSES.IDLE;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.status = STATUSES.ERROR;
+            })
     }
 })
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
+
 // Thunks - middleware to use async call (API)
-export function fetchProducts() {
-    return async function fetchProductsThunk(dispatch, getState) {
-        dispatch(setStatus(STATUSES.LOADING));
-        try {
-            const res = await fetch('https://fakestoreapi.com/products')
-            .then((response) => response.json())
-            .then((data) => { 
-                dispatch(setProducts(data));
-                dispatch(setStatus(STATUSES.IDLE));
-            });
-        }
-        catch(err) {
-            console.log(err);
-            dispatch(setStatus(STATUSES.ERROR));
-        }
-    }
-}
+export const fetchProducts = createAsyncThunk('product/fetch', async () => {
+    const res = await fetch('https://fakestoreapi.com/products')
+    const data = await res.json();
+    return data;
+})
+
+// this is the old way to use thunks (more intuitive).
+// export function fetchProducts() {
+//     return async function fetchProductsThunk(dispatch, getState) {
+//         dispatch(setStatus(STATUSES.LOADING));
+//         try {
+//             const res = await fetch('https://fakestoreapi.com/products')
+//             .then((response) => response.json())
+//             .then((data) => { 
+//                 dispatch(setProducts(data));
+//                 dispatch(setStatus(STATUSES.IDLE));
+//             });
+//         }
+//         catch(err) {
+//             console.log(err);
+//             dispatch(setStatus(STATUSES.ERROR));
+//         }
+//     }
+// }
